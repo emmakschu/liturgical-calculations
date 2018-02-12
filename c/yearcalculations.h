03f3253b82dc91ea2687512c5af83e31b6c58771
Michael K Schumacher <mschu528@gmail.com>
2018-02-12 13:35:18 -0600

    create separate functions to calculate golden num and epacts from either integer year or time tm struct

--- c/yearcalculations.h
+++ c/yearcalculations.h
@@ -1,5 +1,7 @@
 #include <time.h>
 
-int calc_golden_number (struct tm date);
+int calc_golden_number_from_year (int year);
+int calc_golden_number_from_tm (struct tm date);
 
-int calc_epacts (struct tm date);
+int calc_epacts_from_year (int year);
+int calc_epacts_from_tm (struct tm date);
