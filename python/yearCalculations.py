c4e0823299d820a17ee7c322b8b024e57cc54b52
Michael K Schumacher <mschu528@gmail.com>
2018-02-13 13:05:55 -0600

    add function to find Dominical letter

--- python/yearCalculations.py
+++ python/yearCalculations.py
@@ -10,7 +10,7 @@ def calcGoldenNumber(year):
     return golden_num
 
 def calcEpacts(year):
-    """ Returns the epact for a given year
+    """ Returns the epacts for a given year
         The number 0 corresponds to the symbol '*' """
 
     golden_num = calcGoldenNumber(year)
@@ -18,3 +18,58 @@ def calcEpacts(year):
     epact = (golden_num * 11 + 18) % 30
 
     return epact
+
+'''
+To be accurate, Dominical Letter should be calculated by date, not 
+simply year, because in leap years there are two different values -- 
+one before and the other after the Feast of St Matthias
+'''
+def dominicalLetter(date):
+    """ Returns the current Dominical Letter (littera Dominicalis)
+        for a given date. """
+
+    year = date.year
+
+    chartPos = (year - 1901) % 28
+
+    dominicalLetters = [
+            'f',
+            'e',
+            'd',
+            ['c', 'b'],
+            'A',
+            'g',
+            'f',
+            ['e', 'd'],
+            'c',
+            'b',
+            'A',
+            ['g', 'f'],
+            'e',
+            'd',
+            'c',
+            ['b', 'A'],
+            'g',
+            'f',
+            'e',
+            ['d', 'c'],
+            'b',
+            'A',
+            'g',
+            ['f', 'e'],
+            'd',
+            'c',
+            'b',
+            ['A', 'g']
+        ]
+
+    dominicalLetter = dominicalLetters[chartPos]
+
+    if year % 4 == 0 and year % 100 != 0:
+        if date.month <= 2:
+            dominicalLetter = dominicalLetters[chartPos][0]
+        else:
+            dominicalLetter = dominicalLetters[chartPos][1]
+    
+    return dominicalLetter
+
